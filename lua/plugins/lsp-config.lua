912e89e7691059421e5424b4fdab735564df67df
Jeppe <Jeppe.vadandersen@gmail.com>
2025-02-01 16:02:45 +0100

    completions O_O

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,47 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({
+				PATH = "prepend",
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"lua_ls",
+					"stylua",
+					"intelephense",
+					"phpcs",
+					"php-cs-fixer",
+					"clang-format",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
--- lua/plugins/mason.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"williamboman/mason.nvim",
-	config = function()
-		require("mason").setup()
-		PATH = "prepend"
-	end,
-}
